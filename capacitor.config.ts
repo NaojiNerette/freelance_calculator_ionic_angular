0f4f2a5822054dfc307d464b53d8e22b25117142
Joan Bascur <naojimagot@gmail.com>
2024-09-17 11:58:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'freelance-calculator',
+  webDir: 'www'
+};
+
+export default config;
